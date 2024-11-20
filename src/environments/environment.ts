95c8392227d1b94a03370668842b5771805ed4c4
Vishwaslad8549 <mstvishwas@gmail.com>
2024-11-20 13:51:15 +0530

    apiUrl change

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: false,
-    apiUrl: 'http://localhost:3000/api/', // Local API URL
+    apiUrl: 'https://latest-backend-node.onrender.com/api/', // Local API URL
   };
