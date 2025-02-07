c4db7d73d1a52e4584197b93288c5d710c71ffb4
Vishwaslad8549 <mstvishwas@gmail.com>
2025-02-08 00:11:23 +0530

    api url change

--- src/app/services/post.service.ts
+++ src/app/services/post.service.ts
@@ -6,7 +6,7 @@ import { Subject, map } from 'rxjs';
 import { Post } from '../models/posts';
 import { environment } from 'src/environments/environment';
 const url = environment.apiUrl;
-//nst url="http://localhost:3000/api/"
+//const url="http://localhost:3000/api/"
 @Injectable({
   providedIn: 'root'
 })
