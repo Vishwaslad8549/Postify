468884bcec474e5b281fdb90a6c79821526d1c35
Vishwaslad8549 <mstvishwas@gmail.com>
2025-02-17 13:53:47 +0530

    Update post.service.ts

--- src/app/services/post.service.ts
+++ src/app/services/post.service.ts
@@ -5,8 +5,7 @@ import { Router } from '@angular/router';
 import { Subject, catchError, map, throwError } from 'rxjs';
 import { Post } from '../models/posts';
 import { environment } from 'src/environments/environment';
-const url = environment.apiUrl;
-//const url="http://localhost:3000/api/"
+
 import { LoaderService } from './loader.service';
 @Injectable({
   providedIn: 'root'
