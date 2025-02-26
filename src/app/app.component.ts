92cd798d6190d27ba9cda55c954b0d56afc11bbe
Vishwaslad8549 <mstvishwas@gmail.com>
2025-02-27 01:22:15 +0530

    google btn fixes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { PostService } from './services/post.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,10 +8,14 @@ import { PostService } from './services/post.service';
   styleUrls: ['./app.component.scss']
 })
 
-export class AppComponent {
+export class AppComponent implements OnInit{
+  constructor(private router:Router){}
+  ngOnInit(): void {
+    if(localStorage.getItem('token')) 
+      this.router.navigate(['/home'])
+    else this.router.navigate(['']);
+  }
   title = 'latest-angular-postify';
   data: any;
-  constructor(){}
-  oninit(){
-  }
+  
 }
