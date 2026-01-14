88d4155be96047eccb724dcefc577da863502467
unknown <mstvishwas@gmail.com>
2026-01-14 19:34:18 +0530

    changed the hosted backend to AWS.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://latest-backend-node.onrender.com/api/',
+    apiUrl: 'http://testbackend-env.eba-bfkhyuxd.ap-south-1.elasticbeanstalk.com/api/',
      clientId:"805969358163-nt2d6iilr8j129mp3grciue85agkb44p.apps.googleusercontent.com" // Production API URL
   };
   
