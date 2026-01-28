d6fc94cf25a9e8a2898ba459cdea9b9cf34ca530
unknown <mstvishwas@gmail.com>
2026-01-29 00:09:18 +0530

    updated the dev environments file

---
+++ src/environments/environment.dev.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: false,
+    apiUrl: 'http://testbackend-env.eba-bfkhyuxd.ap-south-1.elasticbeanstalk.com/api/',
+     GoogleclientId:"616501876017-2t8gukgmvrrd6cojphujpccm17d7r8p5.apps.googleusercontent.com" // Production API URL
+  };
