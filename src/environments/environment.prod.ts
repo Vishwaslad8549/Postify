faebf5076d4ca19451c9171a1c7365c5bf26fc06
unknown <mstvishwas@gmail.com>
2026-02-01 00:14:22 +0530

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
-    apiUrl: 'http://testbackend-env.eba-bfkhyuxd.ap-south-1.elasticbeanstalk.com/api/',
+    apiUrl: 'http://testbackend-prod-env.eba-bfkhyuxd.ap-south-1.elasticbeanstalk.com/api/',
      GoogleclientId:"616501876017-2t8gukgmvrrd6cojphujpccm17d7r8p5.apps.googleusercontent.com" // Production API URL
   };
   
